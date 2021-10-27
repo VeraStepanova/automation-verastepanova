3ddf155f43eaf181caedbc376776f1e8fc910d31
Vera Stepanova <stepa.vera@gmail.com>
2021-10-27 11:25:40 +0200

    Created a .gitignore and test project

---
+++ frontend-tests/cypress/integration/test-suite.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+//This is test suite
+describe('test suite', function(){
+    
+    //this is a test case
+    it('Perform valid login', function(){
+        cy.visit('http://localhost:3000')
+
+
+    })
+
+})
